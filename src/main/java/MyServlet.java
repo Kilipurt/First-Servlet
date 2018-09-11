9a8b25c25a1e18e9f3c0572885fb16d683477e22
Kilipurt <proandrey15@gmail.com>
2018-09-11 19:51:32 +0300

    init

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,18 +1,24 @@
 import controller.ItemController;
 import entity.Item;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(urlPatterns = "/item")
+//@WebServlet(urlPatterns = "/item")
 public class MyServlet extends HttpServlet {
     private ItemController itemController = new ItemController();
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
