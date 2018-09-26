ceafedebf4e7a6263aaf3ad0e51c09bfc4ef587c
Kilipurt <proandrey15@gmail.com>
2018-09-26 16:24:49 +0300

    add spring framework

---
+++ src/main/java/com/utils/HibernateUtil.java
@@ -0,0 +1,20 @@
+package com.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private SessionFactory sessionFactory;
+
+    public SessionFactory createSessionFactory() {
+        if (sessionFactory == null)
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+
+        return sessionFactory;
+    }
+
+    public void shutDown() {
+        if (sessionFactory != null)
+            sessionFactory.close();
+    }
+}
